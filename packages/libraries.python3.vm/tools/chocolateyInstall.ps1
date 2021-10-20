74d71f958b765c6e00192f206b4b54ec3fc1af3c
MalwareMechanic <blaine.stancill@mandiant.com>
2021-10-20 15:22:27 -0400

    Add try/catch harness and remove broken python libraries.
    Uncompyle6 and Vivisect do not install for Python 2.7, removed them both.
    Added Vivisect to Python3 library list.
    Additionally, added VM-WriteLog messages for failure messages that were previously not being logged.

--- packages/libraries.python3.vm/tools/chocolateyInstall.ps1
+++ packages/libraries.python3.vm/tools/chocolateyInstall.ps1
@@ -23,36 +23,44 @@ function InstallOneModule {
   return $LastExitCode
 }
 
-$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pkgPath = Join-Path $toolDir "packages.json"
+try {
+  $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  $pkgPath = Join-Path $toolDir "packages.json"
 
-$json = LoadPackages $pkgPath
-if (($null -eq $json) -Or ($null -eq $json.packages)) {
-  Write-Host "Error loading packages! Exiting"
-  Exit 1
-}
-
-# Create output file to log python module installation details
-$outputFile = VM-New-Install-Log $toolDir
-
-# upgrade pip
-iex "py -3 -m pip install --no-cache-dir --upgrade pip 2>&1 >> $outputFile"
-
-$packages = $json.packages
-$success = $true
-foreach ($pkg in $packages) {
-  $name = $pkg.name
-  Write-Host "Trying to install $name"
-  if ((InstallOneModule $pkg) -eq 0) {
-    Write-Host "Installed $name"
-  } else {
-    VM-Write-Log "ERROR" "Failed to install $name"
-    $success = $false
+  $json = LoadPackages $pkgPath
+  if (($null -eq $json) -Or ($null -eq $json.packages)) {
+    VM-Write-Log "ERROR" "Error loading packages! Exiting"
+    Exit 1
   }
-}
 
-if ($success -eq $false) {
-  Write-Host "Failed to install at least one Python module"
-  VM-Write-Log "ERROR" "Check $outputFile for more information"
-  Exit 1
+  # Create output file to log python module installation details
+  $outputFile = VM-New-Install-Log $toolDir
+
+  # upgrade pip
+  iex "py -3 -m pip install --no-cache-dir --upgrade pip 2>&1 >> $outputFile"
+
+  $packages = $json.packages
+  $success = $true
+  foreach ($pkg in $packages) {
+    $name = $pkg.name
+    Write-Host "Trying to install $name"
+    if ((InstallOneModule $pkg) -eq 0) {
+      Write-Host "Installed $name"
+    } else {
+      VM-Write-Log "ERROR" "Failed to install $name"
+      $success = $false
+    }
+  }
+
+  if ($success -eq $false) {
+    VM-Write-Log "ERROR" "Failed to install at least one Python module"
+    $outputFile = $outputFile.replace('lib\', 'lib-bad\')
+    VM-Write-Log "ERROR" "Check $outputFile for more information"
+    Exit 1
+  }
+} catch {
+  $msg = $_.Exception.Message
+  $line = $_.InvocationInfo.ScriptLineNumber
+  VM-Write-Log "ERROR" "[Err:$line] $msg"
+  throw
 }
