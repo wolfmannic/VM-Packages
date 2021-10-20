b113beb7ad539e226f7e59f6e5702a0f97437e8a
MalwareMechanic <blaine.stancill@mandiant.com>
2021-10-20 15:25:13 -0400

    Add try/catch harness (not related to rename)

--- packages/python2.x86.vm/tools/chocolateyInstall.ps1
+++ packages/python2.x86.vm/tools/chocolateyInstall.ps1
@@ -1,16 +1,23 @@
 $ErrorActionPreference = 'Stop'
 Import-Module VM.common -Force -DisableNameChecking
 
-$installDir = "${Env:SystemDrive}\Python27.x86" 
-$installArgs  = '/qn /norestart ALLUSERS=1 ADDLOCAL=Extensions TargetDir="{0}"' -f $installDir
+try {
+  $installDir = "${Env:SystemDrive}\Python27.x86"
+  $installArgs  = '/qn /norestart ALLUSERS=1 ADDLOCAL=Extensions TargetDir="{0}"' -f $installDir
 
-$params = @{
-  packageName    = ${Env:ChocolateyPackageName}
-  fileType       = 'msi'
-  silentArgs     = $installArgs
-  url            = 'https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi'
-  checksum       = '1AFA1B10CF491C788BAA340066A813D5EC6232561472CFC3AF1664DBC6F29F77'
-  checksumType   = 'sha256'
+  $params = @{
+    packageName    = ${Env:ChocolateyPackageName}
+    fileType       = 'msi'
+    silentArgs     = $installArgs
+    url            = 'https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi'
+    checksum       = '1AFA1B10CF491C788BAA340066A813D5EC6232561472CFC3AF1664DBC6F29F77'
+    checksumType   = 'sha256'
+  }
+  Install-ChocolateyPackage @params
+  VM-Assert-Path $installDir
+} catch {
+  $msg = $_.Exception.Message
+  $line = $_.InvocationInfo.ScriptLineNumber
+  VM-Write-Log "ERROR" "[Err:$line] $msg"
+  throw
 }
-Install-ChocolateyPackage @params
-VM-Assert-Path $installDir
